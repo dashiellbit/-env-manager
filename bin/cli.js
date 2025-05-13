4be247152fee3d37edb4dcae37eb3aa85e0cb906
dashiellbit <ylhkvktrb978@hotmail.com>
2025-05-13 22:14:36 +0800

    add basic cli structure

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,27 @@
 #!/usr/bin/env node
 
-console.log("env manager v0.1");
+const args = process.argv.slice(2);
+const command = args[0];
+
+if (!command) {
+  console.log("Usage: envmgr <command> [options]");
+  console.log("\nCommands:");
+  console.log("  list     - list all saved environments");
+  console.log("  add      - add new environment");
+  console.log("  load     - load environment variables");
+  process.exit(0);
+}
+
+switch(command) {
+  case 'list':
+    console.log("no environments saved");
+    break;
+  case 'add':
+    console.log("add not implemented yet");
+    break;
+  case 'load':
+    console.log("load not implemented yet");
+    break;
+  default:
+    console.log("unknown command:", command);
+}
