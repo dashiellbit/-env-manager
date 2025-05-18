31ab31d0246988a1a0437b13a02736544b3aa5de
dashiellbit <ylhkvktrb978@hotmail.com>
2025-05-18 16:43:19 +0800

    storage module for config persistence

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const { loadConfig, saveConfig } = require('../lib/storage');
+
 const args = process.argv.slice(2);
 const command = args[0];
 
@@ -14,7 +16,14 @@ if (!command) {
 
 switch(command) {
   case 'list':
-    console.log("no environments saved");
+    const config = loadConfig();
+    const envNames = Object.keys(config.environments);
+    if (envNames.length === 0) {
+      console.log("no environments saved");
+    } else {
+      console.log("Saved environments:");
+      envNames.forEach(name => console.log(`  - ${name}`));
+    }
     break;
   case 'add':
     console.log("add not implemented yet");
